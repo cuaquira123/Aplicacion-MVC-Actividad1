847bedb66b263fb3899e810689ecf7fd07cbc61c
cuaquira123 <rudycuaqyura@gmail.com>
2024-10-12 22:52:58 -0400

    Aplicacion-MVC-Actividad1

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using mvcProgramacionWebII.Models;
+
+namespace mvcProgramacionWebII.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) { }
+
+
+        public DbSet<Producto> Producto { get; set; }
+    }
+}
---
+++ Models/Producto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mvcProgramacionWebII.Models
+{
+    public class Producto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="El nombre es Obligatorio")]
+        [Display(Name = "Nombre del producto")]
+        public string? Name { get; set; }
+        
+
+        [Required(ErrorMessage = "La descripcion es obligatoria")]
+        public string? Description { get; set; }
+    }
+}
